54bcb9759a84fcf7c5c4a6532cdd130dbba68fcc
Kevin Choy <kevinchoy22@gmail.com>
2019-04-01 23:09:14 -0700

    string to integer

--- app1.py
+++ app1.py
@@ -1,2 +1,11 @@
-name = input("What is your name: ")
-print("hi " + name)
+# name = input("What is your name: ")
+# color = input("What is your favorite color: ")
+# print("hi " + name + " your favorite color is " + color)
+
+# birth_year = input("birth year: ")
+# age = 2019 - int(birth_year)
+# print(age)
+
+# weight = input("what is your weight: ")
+# kilo = int(weight) * 0.453592
+# print(kilo)
