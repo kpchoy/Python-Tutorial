9c9ad80fa458bb7ed5391ab424a6989cc2f678d5
Kevin Choy <kevinchoy22@gmail.com>
2019-04-18 16:06:33 -0700

    pahtlib files and directories, line 272

--- app1.py
+++ app1.py
@@ -256,15 +256,21 @@
 # print(leader)
 
 # Dice excercise
-import random
+# import random
 
 
-class Dice:
-    def roll(self):
-        first = random.randint(1, 6)
-        second = random.randint(1, 6)
-        return first, second
+# class Dice:
+#     def roll(self):
+#         first = random.randint(1, 6)
+#         second = random.randint(1, 6)
+#         return first, second
 
 
-dice = Dice()
-print(dice.roll())
+# dice = Dice()
+# print(dice.roll())
+
+# PathLib printing all files in directory
+# from pathlib import Path
+# path = Path()
+# for file in path.glob('*.py'):
+#     print(file)
