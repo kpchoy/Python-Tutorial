7b0173c74b46025c0e542d83b0cb8a2dc1dfd816
Kevin Choy <kevinchoy22@gmail.com>
2019-04-19 16:10:55 -0700

    transactions2 xlsx sheet created with updated price for 10% off

--- transactions.py
+++ transactions.py
@@ -5,4 +5,9 @@ cell = sheet['a1']
 cell = sheet.cell(1, 1)
 
 for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('transactions2.xlsx')
